28d757db3667446455370547613827e70d58614d
Ali Al Kindi <alkindia1@cardiff.ac.uk>
2024-03-26 12:15:46 +0400

    details update

--- components/pokemons/catching.tsx
+++ components/pokemons/catching.tsx
@@ -29,7 +29,7 @@ export default async function CatchingDetails({ poke }: { poke: any }) {
             <TableBody>
               {poke.species.name && (
                 <TableRow>
-                  <TableCell className="w-28 font-bold">Species</TableCell>
+                  <TableCell className="w-32 font-bold">Species</TableCell>
                   <TableCell className="capitalize">
                     {poke.species.name}
                   </TableCell>
@@ -38,35 +38,37 @@ export default async function CatchingDetails({ poke }: { poke: any }) {
 
               {poke.weight && (
                 <TableRow>
-                  <TableCell className="w-28 font-bold">Weight</TableCell>
-                  <TableCell>{poke.weight * 0.1} kg</TableCell>
+                  <TableCell className="w-32 font-bold">Weight</TableCell>
+                  <TableCell>{(poke.weight * 0.1).toFixed(1)} kg</TableCell>
                 </TableRow>
               )}
 
               {poke.height && (
                 <TableRow>
-                  <TableCell className="w-28 font-bold">Height</TableCell>
-                  <TableCell>{poke.height * 10} cm</TableCell>
+                  <TableCell className="w-32 font-bold">Height</TableCell>
+                  <TableCell>{(poke.height * 10).toFixed(1)} cm</TableCell>
                 </TableRow>
               )}
 
               {poke.base_experience && (
                 <TableRow>
-                  <TableCell className="w-28 font-bold">Base Exp.</TableCell>
+                  <TableCell className="w-32 font-bold">Base Exp.</TableCell>
                   <TableCell>{poke.base_experience}</TableCell>
                 </TableRow>
               )}
 
-              {species.captureRate && (
+              {species.capture_rate && (
                 <TableRow>
-                  <TableCell className="w-28 font-bold">Catch Rate</TableCell>
-                  <TableCell>{species.captureRate}</TableCell>
+                  <TableCell className="w-32 font-bold">Catch Rate</TableCell>
+                  <TableCell>
+                    {((species.capture_rate / 255) * 100).toFixed(1)}%
+                  </TableCell>
                 </TableRow>
               )}
 
               {species.egg_groups && (
                 <TableRow>
-                  <TableCell className="w-28 font-bold">Egg Groups</TableCell>
+                  <TableCell className="w-32 font-bold">Egg Groups</TableCell>
                   <TableCell>
                     {species.egg_groups
                       .map((group: { name: string }) => (
@@ -85,7 +87,7 @@ export default async function CatchingDetails({ poke }: { poke: any }) {
 
               {species.growth_rate && (
                 <TableRow>
-                  <TableCell className="w-28 font-bold">Growth</TableCell>
+                  <TableCell className="w-32 font-bold">Growth Rate</TableCell>
                   <TableCell className="capitalize">
                     {species.growth_rate.name}
                   </TableCell>
@@ -94,8 +96,16 @@ export default async function CatchingDetails({ poke }: { poke: any }) {
 
               {species.gender_rate && (
                 <TableRow>
-                  <TableCell className="w-28 font-bold">Gender Rate</TableCell>
-                  <TableCell>{species.gender_rate}</TableCell>
+                  <TableCell className="w-32 font-bold">Gender Ratio</TableCell>
+                  <TableCell>
+                    <span className="text-blue-500">
+                      {((8 - species.gender_rate) / 8) * 100}% Male
+                    </span>
+                    {", "}
+                    <span className="text-pink-400">
+                      {(species.gender_rate / 8) * 100}% Female
+                    </span>
+                  </TableCell>
                 </TableRow>
               )}
             </TableBody>
