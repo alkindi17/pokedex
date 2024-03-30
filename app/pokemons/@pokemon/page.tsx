5dc5bda1f71338f4ec14c233eae3082efea187ee
Ali Al Kindi <alkindia1@cardiff.ac.uk>
2024-03-31 01:21:04 +0400

    favicon and pokeball.svg update

--- app/pokemons/@pokemon/page.tsx
+++ app/pokemons/@pokemon/page.tsx
@@ -3,7 +3,7 @@ import PokeBall from "@/public/svg/pokeball.svg";
 export default function Pokemon() {
   return (
     <div className="flex h-full flex-col justify-center bg-[#f5f5f5] align-middle">
-      <PokeBall className="mx-auto h-96 w-96 opacity-5" />
+      <PokeBall className="mx-auto h-64 w-64 opacity-5" />
     </div>
   );
 }
