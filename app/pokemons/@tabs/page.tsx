eb1ce84d622c0629c810d6ba0fa11ce8e5b328fd
Ali Al Kindi <alkindia1@cardiff.ac.uk>
2024-04-01 01:16:45 +0400

    cleaned hello

--- app/pokemons/@tabs/page.tsx
+++ app/pokemons/@tabs/page.tsx
@@ -1,11 +1,5 @@
 "use client";
 
 export default function Tabs() {
-  return (
-    <>
-      <div className="flex h-full flex-col justify-center bg-[#f5f5f5] align-middle">
-        Hello
-      </div>
-    </>
-  );
+  return <></>;
 }
