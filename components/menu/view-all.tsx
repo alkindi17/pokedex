fac2ccdf3d4814b0213a53d21cd8bfa581ca2c09
Ali Al Kindi <alkindia1@cardiff.ac.uk>
2024-04-03 17:09:04 +0400

    Update styles for ViewAll component

--- components/menu/view-all.tsx
+++ components/menu/view-all.tsx
@@ -7,8 +7,8 @@ import Icon from "@/public/svg/pokeball2.svg";
 export default function ViewAll() {
   return (
     <div className="transition hover:scale-[1.01]">
-      <Link href="/pokemons/list" className="relative">
-        <Card className="mx-4 mt-2 flex justify-between overflow-clip bg-[#E67F70] from-red-700 to-red-400 text-xl text-white">
+      <Link href="/pokemons/list" className="">
+        <Card className="relative mx-4 mt-2 flex justify-between overflow-hidden bg-[#E67F70] from-red-700 to-red-400 text-xl text-white">
           <div className="h-20">
             <div className="flex  items-center gap-2 px-3 py-2">
               <p className="text-2xl font-bold">View all pokemons</p>
@@ -16,7 +16,7 @@ export default function ViewAll() {
             </div>
           </div>
           <Icon
-            className="absolute -right-2 top-0 h-32 w-32 rotate-12 opacity-15"
+            className="absolute right-0 top-0 h-32 w-32 translate-x-6 rotate-12 opacity-15"
             fill={"white"}
           />
         </Card>
